6119b002b18b6f47182f219d579a9be7df259878
brina-cloud <s.kaliza1@alustudent.com>
2025-10-16 18:13:32 +0200

    second task

--- create_environment.sh
+++ create_environment.sh
@@ -1,78 +1,80 @@
 #!/bin/bash
 
 #Create the directory .
-
-
 read -p " Enter your name: " yourName
-if [ ! -d ~/submission_reminder_$yourName/ ]; then
 
-  mkdir ~/submission_reminder_$yourName/
+
+dir=~/submission_reminder_app_Brina-cloud/submission_reminder_$yourName
+
+if [ ! -d $dir/ ]; then
+
+  mkdir $dir/
   echo -e "\nDIRECTORY ~/submission_reminder_$yourname IS CREATED .....\n"
 else
   echo -e "DIRECTORY ARLEADY EXISTS"
 fi
 
-  cd ~/submission_reminder_$yourName/
+  cd $dir/
 
-  if [ ! -d  ~/submission_reminder_$yourName/app ]; then
+  if [ ! -d  $dir/app ]; then
 
    echo -e "Creating the app directory \n"
-   mkdir -p ~/submission_reminder_$yourName/app
+   mkdir -p $dir/app
 
   else
      echo -e "APP directory already exists. \n"
 
   fi
-   touch ~/submission_reminder_$yourName/app/reminder.sh
+   touch $dir/app/reminder.sh
 
 #Creating modules directory
 
-   if [ ! -d  ~/submission_reminder_$yourName/modules/ ]; then
+   if [ ! -d  $dir/modules/ ]; then
 
     echo -e "Creating the modules directory.... \n"
-    mkdir -p ~/submission_reminder_$yourName/modules/
+    mkdir -p $dir/modules/
 
    else
      echo -e "MODULES directory already exists. \n"
 
    fi
-   touch ~/submission_reminder_$yourName/modules/functions.sh
+   touch $dir/modules/functions.sh
 
 
 # Creating assets directory
 
-  if [ ! -d  ~/submission_reminder_$yourName/assets/ ]; then
+  if [ ! -d  $dir/assets/ ]; then
 
    echo -e "Creating the assets directory.... \n"
-   mkdir -p ~/submission_reminder_$yourName/assets/
+   mkdir -p $dir/assets/
 
   else
      echo -e "APP directory already exists. \n"
 
   fi
-   touch ~/submission_reminder_$yourName/assets/submissions.txt
+   touch $dir/assets/submissions.txt
 
 
  #Creating the config directory.
 
-  if [ ! -d  ~/submission_reminder_$yourName/config/ ]; then
+  if [ ! -d  $dir/config/ ]; then
 
    echo -e "Creating the config directory....\n"
-   mkdir -p ~/submission_reminder_$yourName/config/
+   mkdir -p $dir/config/
 
   else
      echo -e "CONFIG directory already exists. \n"
 
   fi
-   touch ~/submission_reminder_$yourName/config/config.env
+   touch $dir/config/config.env
 
 
   # Creating the startup.sh file
 
-  if [ ! -f  ~/submission_reminder_$yourName/startup.sh ]; then
+  if [ ! -f  $dir/startup.sh ]; then
 
    echo -e "Creating the startup.sh file.... \n"
-   touch ~/submission_reminder_$yourName/startup.sh
+   touch $dir/startup.sh
 
   else
      echo -e "The startup.sh file already exists. \n"
