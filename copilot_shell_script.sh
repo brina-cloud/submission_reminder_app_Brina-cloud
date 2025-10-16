3d0045be74671c28dab5802030cda9a5c0b81de8
brina-cloud <s.kaliza1@alustudent.com>
2025-10-16 18:13:32 +0200

    second task

---
+++ copilot_shell_script.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#Replace assignment name in config/config.env
+
+
+loc=~/submission_reminder_app_Brina-cloud/submission_reminder_Sabrina/config/config.env
+# sourcing the config.env 
+source "$loc"
+# To input the new assignment 
+
+read -p "Enter the new assignment: " new
+
+if [[ $ASSIGNMENT == $new ]]; then
+   echo -e "WRONG ASSIGNMENT, Enter the another assignment"
+else
+   echo -e "....Updating assignment to $new "
+fi
+
+# Change the value in variable ASSIGNMENT
+ sed -i "s/^ASSIGNMENT=.*/ASSIGNMENT=\"$new\"/" "$loc"
+
+
